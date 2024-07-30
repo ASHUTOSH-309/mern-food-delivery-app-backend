fbf7f1f6074197e908b51256fdd488073d145c97
ASHUTOSH-309 <ashutosh.sharma83020@gmail.com>
2024-07-30 17:11:54 +0530

    End point for restaurant Info

--- src/controllers/RestaurantController.ts
+++ src/controllers/RestaurantController.ts
@@ -1,6 +1,30 @@
 import { Request, Response } from "express";
 import Restaurant from "../models/restaurant";
 
+
+const getRestaurant=async (req:Request,res:Response)=>{
+
+    try {
+
+        const restaurantId=req.params.restaurantId;
+        const restaurant=await Restaurant.findById(restaurantId)
+
+        if(!restaurant){
+            return res.status(404).json({message:"restaurant not found"})
+        }
+        res.json(restaurant)
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message:"Something went wrong"})
+    }
+
+
+
+}
+
+
+
 const searchRestaurants=async(req:Request,res:Response)=>{
 
     try {
@@ -75,6 +99,7 @@ const searchRestaurants=async(req:Request,res:Response)=>{
 export default {
 
     searchRestaurants,
+    getRestaurant,
 
 
 }
--- src/routes/RestaurantRoute.ts
+++ src/routes/RestaurantRoute.ts
@@ -5,6 +5,19 @@ import RestaurantController from "../controllers/RestaurantController";
 
 const router=express.Router()
 
+
+router.get("/:restaurantId",
+    param("restaurantId")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("Restaurant ID parameter must be a valid parameter"),
+    RestaurantController.getRestaurant
+)
+
+
+
+
 router.get("/search/:city",
     param("city")
     .isString()
